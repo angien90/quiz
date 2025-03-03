59c151497580b4b5318c93d2710c7ac09ae22b21
angien90 <angelicanylander@hotmail.com>
2025-03-03 11:49:27 +0100

    npm, sass installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  'base': '/angien90/',
-  //css: {
-  //  preprocessorOptions: {
-  //   scss: {
-  //      additionalData: `@import "./src/scss/abstracts/variables/_mx-*";`
-  //    }
-  //  }
-  // }
+  resolve: {
+    extensions: ['.ts', '.js', '.scss'] // Lägg till '.scss' här
+  }
 });
