47f4233593cbfce242aa94e427515436b70cee06
angien90 <angelicanylander@hotmail.com>
2025-03-03 11:01:53 +0100

    Change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  'base': '/fed24d-js-intro-inl-2-quiz-jazzy/',
+  'base': '/angien90/',
   //css: {
   //  preprocessorOptions: {
   //   scss: {
